582f1015c43da99de7d4de76ddfbb7563979fee2
melkam <melkamuhun@gmail.com>
2023-02-28 20:59:35 +0300

    feat: add app componet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductItemDetailComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'confirmation', component: ConfirmationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CartComponent } from './components/cart/cart.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CartItemComponent } from './components/cart/cart-item/cart-item.component';
+import { CheckoutFormComponent } from './components/cart/checkout-form/checkout-form.component';
+import { AddtocartComponent } from './components/addtocart/addtocart.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ConfirmationComponent,
+    ProductItemComponent,
+    ProductListComponent,
+    ProductItemDetailComponent,
+    HeaderComponent,
+    CartItemComponent,
+    CheckoutFormComponent,
+    AddtocartComponent,
   ],
   imports: [
+    FormsModule,
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
